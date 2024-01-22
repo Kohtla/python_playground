ff0492fabd3657ef8e7b072ad2b2870d9b8f2b0c
Alex <MKmoolooo@yandex.ru>
2024-01-22 15:35:18 +0400

    some fixes

--- thread_server.py
+++ thread_server.py
@@ -1,8 +1,9 @@
-from socket import *
+from socket import socket, AF_INET, SOCK_STREAM
 from main import fib
 import threading
 
-SERVER_ADRESS = ('localhost', 25000)
+SERVER_ADRESS = ("localhost", 25000)
+
 
 def run():
     server_socket = socket(AF_INET, SOCK_STREAM)
@@ -12,11 +13,12 @@ def run():
     print(f"Server listening on {SERVER_ADRESS}")
 
     while True:
-        client_socket, client_address = server_socket.accept() # Blocking
-        print(f'Connection with {client_address} established')        
-        
-        thread = threading.Thread(target=handle_connection, args=(client_socket, ))
+        client_socket, client_address = server_socket.accept()  # Blocking
+        print(f"Connection with {client_address} established")
+
+        thread = threading.Thread(target=handle_connection, args=(client_socket,))
         thread.start()
+
 
 def handle_connection(client_socket):
     while True:
@@ -25,10 +27,11 @@ def handle_connection(client_socket):
             response = int(response)
             response = fib(response)
             response = str(response)
-        except:
-            response = ''
-        response = response.encode()       
+        except Exception:
+            response = ""
+        response = response.encode()
         client_socket.sendall(response)
+
 
 if __name__ == "__main__":
     run()
