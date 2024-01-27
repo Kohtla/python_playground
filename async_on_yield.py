b5296332e62344af9bb831c5f9a134e17fb53a9b
Alex <MKmoolooo@yandex.ru>
2024-01-27 16:58:29 +0400

    comments

--- async_on_yield.py
+++ async_on_yield.py
@@ -2,6 +2,7 @@ import time
 import random
 
 
+# 'async' non blocking function
 def timed_process(execution_time):
     start = time.time()
     while True:
@@ -12,8 +13,10 @@ def timed_process(execution_time):
             break
 
 
+# list of async funcs
 processes = [timed_process(random.randint(1, 10)) for _ in range(100)]
 
+# Execute funcs and remove from the waitlist
 start = time.time()
 while True:
     print(f"Processes count: {len(processes)}")
